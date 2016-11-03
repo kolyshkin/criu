0bc575700f82bb0596f30936d711326850fbf3d1
Kir Kolyshkin <kir@openvz.org>
2016-11-03 15:22:41 -0700

    [arm] fix asm syntax to be clang-compatible
    I'm unsure where all these %%s before register names comes from,
    but they confuse clang, like this:
    
    > criu/arch/arm/parasite-head.S:6:6: error: unexpected token in operand
    >  sub %r2, %pc, #8 @ get the address of this instruction
    >     ^
    
    This patch looks scary, but all it does is removing %s before
    register names, fixing a few "many spaces instead of a tab" issues
    along the way.
    
    Cc: Christopher Covington <cov@codeaurora.org>
    Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- criu/arch/arm/include/asm/restorer.h
+++ criu/arch/arm/include/asm/restorer.h
@@ -10,28 +10,28 @@
 			     thread_args, clone_restore_fn)		\
 	asm volatile(							\
 		     "clone_emul:				\n"	\
-		     "ldr %%r1, %2				\n"	\
-		     "sub %%r1, #16				\n"	\
-		     "mov %%r0, %%%6				\n"	\
-		     "str %%r0, [%%r1, #4]			\n"	\
-		     "mov %%r0, %%%5				\n"	\
-		     "str %%r0, [%%r1]				\n"	\
-		     "mov %%r0, %%%1				\n"	\
-		     "mov %%r2, %%%3				\n"	\
-		     "mov %%r3, %%%4				\n"	\
-		     "mov %%r7, #"__stringify(__NR_clone)"	\n"	\
+		     "ldr r1, %2				\n"	\
+		     "sub r1, #16				\n"	\
+		     "mov r0, %6				\n"	\
+		     "str r0, [r1, #4]				\n"	\
+		     "mov r0, %5				\n"	\
+		     "str r0, [r1]				\n"	\
+		     "mov r0, %1				\n"	\
+		     "mov r2, %3				\n"	\
+		     "mov r3, %4				\n"	\
+		     "mov r7, #"__stringify(__NR_clone)"	\n"	\
 		     "svc #0					\n"	\
 									\
-		     "cmp %%r0, #0				\n"	\
+		     "cmp r0, #0				\n"	\
 		     "beq thread_run				\n"	\
 									\
-		     "mov %%%0, %%r0				\n"	\
+		     "mov %0, r0				\n"	\
 		     "b   clone_end				\n"	\
 									\
 		     "thread_run:				\n"	\
-		     "pop { %%r1 }				\n"	\
-		     "pop { %%r0 }				\n"	\
-		     "bx  %%r1					\n"	\
+		     "pop { r1 }				\n"	\
+		     "pop { r0 }				\n"	\
+		     "bx  r1					\n"	\
 									\
 		     "clone_end:				\n"	\
 		     : "=r"(ret)					\
@@ -45,9 +45,9 @@
 
 #define ARCH_FAIL_CORE_RESTORE					\
 	asm volatile(						\
-		     "mov %%sp, %0			    \n"	\
-		     "mov %%r0, #0			    \n"	\
-		     "bx  %%r0				    \n"	\
+		     "mov sp, %0			\n"	\
+		     "mov r0, #0			\n"	\
+		     "bx  r0				\n"	\
 		     :						\
 		     : "r"(ret)					\
 		     : "memory")
@@ -61,12 +61,12 @@ int restore_nonsigframe_gpregs(UserArmRegsEntry *r);
 
 static inline void restore_tls(tls_t *ptls) {
 	asm (
-	     "mov %%r7, #15  \n"
-	     "lsl %%r7, #16  \n"
-	     "mov %%r0, #5   \n"
-	     "add %%r7, %%r0 \n"	/* r7 = 0xF005 */
-	     "ldr %%r0, [%0] \n"
-	     "svc #0         \n"
+	     "mov r7, #15	\n"
+	     "lsl r7, #16	\n"
+	     "mov r0, #5	\n"
+	     "add r7, r0	\n"	/* r7 = 0xF005 */
+	     "ldr r0, [%0]	\n"
+	     "svc #0		\n"
 	     :
 	     : "r"(ptls)
 	     : "r0", "r7"
