082f153c6d5890100c78dc89c2b20d5843e2918f
Kir Kolyshkin <kir@openvz.org>
2016-11-03 21:22:39 -0700

    arm64: provide own memcpy for pie
    Got the following error trying to compile this with clang-3.6 on arm64:
    
    >   GEN      criu/pie/restorer.built-in.bin.o
    > criu/pie/native.lib.a(parasite-vdso.o): In function `vdso_proxify':
    > /criu/criu/pie/parasite-vdso.c:132: undefined reference to `memcpy'
    
    This happens because clang emits a call to memcpy for struct
    initialization (specifically, struct vdso_symtable in vdso_proxify()).
    
    See commit 214e280 ("pie: provide own memcpy for x86") for more
    background info about the issue.
    
    Cc: Christopher Covington <cov@codeaurora.org>
    Cc: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- criu/arch/aarch64/include/asm/string.h
+++ criu/arch/aarch64/include/asm/string.h
@@ -1,7 +1,21 @@
 #ifndef __CR_ASM_STRING_H__
 #define __CR_ASM_STRING_H__
 
+#define HAS_BUILTIN_MEMCPY
+
 #include "common/compiler.h"
 #include "asm-generic/string.h"
+
+#ifdef CR_NOGLIBC
+extern void *memcpy_arm64(void *to, const void *from, size_t n);
+static inline void *builtin_memcpy(void *to, const void *from, size_t n)
+{
+	if (n)
+		memcpy_arm64(to, from, n);
+	return to;
+}
+#else
+#define builtin_memcpy memcpy
+#endif /* CR_NOGLIBC */
 
 #endif /* __CR_ASM_STRING_H__ */
