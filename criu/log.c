5ded236e3e0f63ad56c8b326ee8ac35cf7e6f828
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-11-08 19:55:00 +0300

    criu: call compel_log_init() at start
    Otherwise, messages printed by libcompel are omitted from logs.
    
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/log.c
+++ criu/log.c
@@ -212,7 +212,7 @@ unsigned int log_get_loglevel(void)
 	return current_loglevel;
 }
 
-static void __print_on_level(unsigned int loglevel, const char *format, va_list params)
+void vprint_on_level(unsigned int loglevel, const char *format, va_list params)
 {
 	int fd, size, ret, off = 0;
 	int __errno = errno;
@@ -249,7 +249,7 @@ void print_on_level(unsigned int loglevel, const char *format, ...)
 	va_list params;
 
 	va_start(params, format);
-	__print_on_level(loglevel, format, params);
+	vprint_on_level(loglevel, format, params);
 	va_end(params);
 }
 
