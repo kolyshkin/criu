0d514b092e89ce039547bf83d5d02922e2547eb0
Kirill Tkhai <ktkhai@virtuozzo.com>
2016-11-30 19:28:58 +0300

    files: Create per-process transport sock and bind it
    This will be used instead of currently existing per-fd
    transport sockets, which are used on receive_fd stage.
    
    v2: Create the socket before CR_STATE_FORKING state.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -272,10 +272,6 @@ static int root_prepare_shared(void)
 	if (ret)
 		goto err;
 
-	ret = open_transport_socket();
-	if (ret)
-		goto err;
-
 	show_saved_files();
 err:
 	return ret;
@@ -715,8 +711,6 @@ static int restore_one_alive_task(int pid, CoreEntry *core)
 
 	if (prepare_vmas(current, ta))
 		return -1;
-
-	close_service_fd(TRANSPORT_FD_OFF);
 
 	if (setup_uffd(pid, ta))
 		return -1;
@@ -1463,6 +1457,9 @@ static int restore_task_with_children(void *_arg)
 
 		fini_restore_mntns();
 	}
+
+	if (open_transport_socket())
+		return -1;
 
 	if (restore_finish_stage(task_entries, CR_STATE_FORKING) < 0)
 		goto err;
