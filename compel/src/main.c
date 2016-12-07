f48b3df948098710fb1087c12b60db94717d6c09
Kir Kolyshkin <kir@openvz.org>
2016-12-07 14:28:11 -0800

    compel hgen: drop -u option
    This -u option always looked wrong to me, I mean, how the hell a user
    is supposed to know where the hell those headers are? It took quite
    a while to figure out what to do with it, but the end result is --
    this option is not needed at all and can easily be dropped.
    
    For finding paths to includes, there is a -I compiler option,
    there's no need to specify something to compel.
    
    In fact, it should know by itself where its own headers are kept
    (and emit -I... to cflags if needed), but that's another story
    which is to be told when we'll decide to pack compel as a standalone
    tool. For now, just add "#include <compel/compel.h>" and be done.
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- compel/src/main.c
+++ compel/src/main.c
@@ -29,7 +29,6 @@
 
 piegen_opt_t opts = {
 	.input_filename		= NULL,
-	.uapi_dir		= "piegen/uapi",
 	.fout			= NULL,
 };
 
@@ -117,12 +116,11 @@ int main(int argc, char *argv[])
 		},
 	};
 
-	static const char short_opts[] = "a:f:o:u:p:hVl:";
+	static const char short_opts[] = "a:f:o:p:hVl:";
 	static struct option long_opts[] = {
 		{ "arch",	required_argument,	0, 'a' },
 		{ "file",	required_argument,	0, 'f' },
 		{ "output",	required_argument,	0, 'o' },
-		{ "uapi-dir",	required_argument,	0, 'u' },
 		{ "prefix",	required_argument,	0, 'p' },
 		{ "help",	required_argument,	0, 'h' },
 		{ "version",	no_argument,		0, 'V' },
@@ -155,9 +153,6 @@ int main(int argc, char *argv[])
 			break;
 		case 'o':
 			opts.output_filename = optarg;
-			break;
-		case 'u':
-			opts.uapi_dir = optarg;
 			break;
 		case 'p':
 			opts.prefix = optarg;
