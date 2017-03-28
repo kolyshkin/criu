e6b1c70fe18ab5bc8c8a7282f73806b56baa5a75
Kir Kolyshkin <kir@openvz.org>
2017-03-28 11:36:53 -0700

    Mismatched pid debug
    Debug for https://github.com/xemul/criu/issues/282
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -1351,6 +1351,8 @@ static int restore_task_with_children(void *_arg)
 	pid = getpid();
 	if (vpid(current) != pid) {
 		pr_err("Pid %d do not match expected %d\n", pid, vpid(current));
+		int n = system("ps axf");
+		pr_debug("ps axf ret=%d\n", n);
 		set_task_cr_err(EEXIST);
 		goto err;
 	}
